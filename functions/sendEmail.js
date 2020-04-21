d9bf40c83f169781999c4c59e58364a65ed171d0
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-04-21 15:11:42 -0700

    email

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,26 +1,30 @@
 const sgMail = require("@sendgrid/mail");
 const { SENDGRID_API_KEY } = process.env;
 
-exports.handler = async () => {
-  sgMail.setApiKey(SENDGRID_API_KEY);
-  const msg = {
-    to: "macguire.rintoul@gmail.com",
-    from: "mrintoul@sfu.ca",
-    subject: "Contact Form Submission",
-    text: "It works"
-  };
-
-  try {
-    await sgMail.send(msg);
-
-    return {
-      statusCode: 200,
-      body: "Message sent"
-    };
-  } catch (e) {
-    return {
-      statusCode: e.code,
-      body: e.message
-    };
+exports.handler = async request => {
+	sgMail.setApiKey(SENDGRID_API_KEY);
+  let data = JSON.parse(request.body);
+  
+	const msg = {
+		to: "macguire.rintoul@gmail.com",
+		from: "mrintoul@sfu.ca",
+		subject: "Form Submission",
+		text: `
+    Name: ${data.fullName}
+    `
+	};
+	
+	try {
+		await sgMail.send(msg);
+		return {
+			statusCode: 200,
+			body: "Message sent"
+		};
+	} catch (e) {
+		return {
+			statusCode: e.code,
+			body: e.message
+		};
   }
+  */
 };
--- src/pages/sign-up.vue
+++ src/pages/sign-up.vue
@@ -1,55 +1,121 @@
 <template>
-  <Layout>
-    <h1>Sign up</h1>
-    <p>
-      Thank you for your interest in Blue Moon Organics. Please complete the
-      Sign-up form and we will contact you to confirm your first delivery date.
-    </p>
-    <form>
-      <label>
-        Full name*
-        <input type="text" name="name" required/>
-      </label>
+	<Layout>
+		<h1>Sign up</h1>
+		<p>
+			Thank you for your interest in Blue Moon Organics. Please complete the
+			Sign-up form and we will contact you to confirm your first delivery date.
+		</p>
+		<form>
+			<label for="fullName">Full name*</label>
+			<input
+				placeholder="John Appleseed"
+				v-model="fullName"
+				type="text"
+				name="fullName"
+			/>
 
-      <label>
-        Phone number*
-        <input type="tel" name="phone" required />
-      </label>
+			<label for="phone">Phone number*</label>
+			<input
+				placeholder="123-123-1234"
+				v-model="phone"
+				type="tel"
+				name="phone"
+			/>
 
-      <label>
-        Address - line 1*
-        <input type="text" name="address1" required />
-      </label>
+			<label for="address1">Address - line 1*</label>
+			<input
+				placeholder="502 Infinite Way"
+				v-model="address1"
+				type="text"
+				name="address1"
+			/>
 
-      <label>
-        Address - line 2
-        <input type="text" name="address2" />
-      </label>
+			<label for="address2">Address - line 2</label>
+			<input
+				placeholder="Unit 15"
+				v-model="address2"
+				type="text"
+				name="address2"
+			/>
 
-      <label>
-        City*
-        <input type="text" name="city" required />
-      </label>
+			<label for="city">City*</label>
+			<input placeholder="Coquitlam" v-model="city" type="text" name="city" />
 
-      <label>
-        Email address*
-        <input type="email" name="email" required />
-      </label>
+			<label for="email">Email address*</label>
+			<input
+				placeholder="john@appleseed.com"
+				v-model="email"
+				type="email"
+				name="email"
+			/>
 
-      <label>How often would you like delivery?*</label>
-      <input id="everyweek" type="radio" name="frequency" value="Every week" required />
-      <label class="radio" for="everyweek">Every week</label>
+			<label>How often would you like delivery?*</label>
+			<input
+				id="everyweek"
+				type="radio"
+				name="frequency"
+				value="Every week"
+				v-model="frequency"
+			/>
+			<label class="radio" for="everyweek">Every week</label>
 
-      <input id="everyotherweek" type="radio" name="frequency" value="Every other week" />
-      <label class="radio" for="everyotherweek">Every other week</label>
+			<input
+				id="everyotherweek"
+				type="radio"
+				name="frequency"
+				v-model="frequency"
+				value="Every other week"
+			/>
+			<label class="radio" for="everyotherweek">Every other week</label>
 
-      <label for="startdate">Start date*</label>
-      <input type="date" name="startdate" required />
+			<label for="startDate">Start date*</label>
+			<input v-model="startDate" type="date" name="startDate" />
 
-      <label for="promocode">Promo code</label>
-      <input type="text" name="promocode" />
+			<label for="promocode">Promo code</label>
+			<input v-model="promoCode" type="text" name="promoCode" />
 
-      <button type="submit">Submit</button>
-    </form>
-  </Layout>
+			<!-- the following input is a honeypot -->
+			<label for="captcha"></label>
+			<input v-model="captcha" type="text" name="captcha" id="captcha" />
+
+			<button @click="submit" type="submit">Submit</button>
+		</form>
+	</Layout>
 </template>
+
+<script>
+export default {
+	methods: {
+		submit(event) {
+			event.preventDefault();
+			console.log("Submitting...");
+			let data = {
+				fullName: this.fullName
+			};
+			fetch(".netlify/functions/sendEmail", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(data)
+			})
+				.then(response => console.log(response.data))
+				.catch(error => console.error(error));
+		}
+	},
+	data() {
+		return {
+			fullName: "",
+			phone: "",
+			address1: "",
+			address2: "",
+			city: "",
+			frequency: "",
+			email: "",
+			startDate: "",
+			promoCode: "",
+			captcha: ""
+		};
+	}
+};
+</script>
