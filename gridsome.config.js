e278b4fac5f9254bc8cc0f1ec9d2343d62b6f3d1
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-05-03 13:06:00 -0700

    add delivery areas

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,15 +1,23 @@
 // Changes here require a server restart.
 
 module.exports = {
-  siteName: "Blue Moon Organics",
-  plugins: [
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        baseDir: "./content/pages",
-        typeName: "SitePage",
-        path: "*.md"
-      }
-    }
-  ]
+	siteName: "Blue Moon Organics",
+	plugins: [
+		{
+			use: "@gridsome/source-filesystem",
+			options: {
+				baseDir: "./content/pages",
+				typeName: "SitePage",
+				path: "*.md"
+			}
+		},
+		{
+			use: "@gridsome/source-filesystem",
+			options: {
+				baseDir: "./content/delivery-areas",
+				typeName: "DeliveryArea",
+				path: "*.md"
+			}
+		}
+	]
 };
---
+++ src/templates/DeliveryArea.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="content">
+		<h1>{{ $page.deliveryArea.title }}</h1>
+		<p>We deliver to {{ $page.deliveryArea.title }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	metaInfo() {
+		return {
+			title: this.$page.deliveryArea.title
+		};
+	}
+};
+</script>
+
+<page-query>
+query DeliveryArea($id: ID!) {
+  deliveryArea: deliveryArea(id: $id) {
+    title
+  }
+}
+</page-query>
